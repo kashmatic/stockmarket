e9ed9dec5ffe69a6170533dc6c1ac8c713527e29
Kashi Revanna <kashi.revanna@verizonwireless.com>
2018-02-09 22:36:27 -0600

    database

---
+++ stockprice/006_search.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+
+from lib.iex import IEX
+from lib.iex_criteria import IexCriteria
+
+client = MongoClient("mongodb://localhost:27017/stocks")
+db = client['stocks']
+GOOD = open("search.out", "w")
+BAD = open("search.err", "w")
+
+
+def criteria(symbol):
+    aobj = IexCriteria(symbol,
+        db[symbol].find_one({},{"stocksEarnings":1, "_id":0})['stocksEarnings'],
+        db[symbol].find_one({},{"stocksFinancials":1, "_id":0})['stocksFinancials'],
+        db[symbol].find_one({},{"stocksKeyStats":1, "_id":0})['stocksKeyStats'],
+        db[symbol].find_one({},{"stocksQuote":1, "_id":0})['stocksQuote'])
+    abool, msg = aobj.validate()
+    if abool:
+        GOOD.write("{}\t{}\n".format(symbol, msg))
+    else:
+        BAD.write("{}\t{}\n".format(symbol, msg))
+
+def each_symbol():
+    for sym in db.collection_names():
+        print(sym)
+        criteria(sym)
+
+if __name__ == "__main__":
+    each_symbol()
+    # criteria('A')
+    GOOD.close()
+    BAD.close()
