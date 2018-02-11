4e3509352cb60ff77683429dfa8f091d3a23103e
Kashi Revanna <kashi.revanna@verizonwireless.com>
2018-02-11 17:16:27 -0600

    (update) finviz data

--- stockprice/005_database.py
+++ stockprice/005_database.py
@@ -1,21 +1,37 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 from lib.iex import IEX
+from lib.finviz import Finviz
 
 client = MongoClient("mongodb://localhost:27017/stocks")
-db = client['stocks']
+DB = client['stocks']
 
-iex = IEX()
+def iex_database(alist):
+    for sym in alist:
+        if not sym.startswith('A'):
+            continue
+        print(sym)
+        stocksEarnings = iex.stocksEarnings(sym)
+        stocksFinancials = iex.stocksFinancials(sym)
+        stocksKeyStats = iex.stocksKeyStats(sym)
+        stocksQuote = iex.stocksQuote(sym)
+        DB[sym].insert({'stocksKeyStats': stocksKeyStats,
+            'stocksFinancials': stocksFinancials,
+            'stocksEarnings': stocksEarnings,
+            'stocksQuote': stocksQuote})
 
-for sym in iex.symbols():
-    # if not sym.startswith('A'):
-    #     continue
-    print(sym)
-    stocksEarnings = iex.stocksEarnings(sym)
-    stocksFinancials = iex.stocksFinancials(sym)
-    stocksKeyStats = iex.stocksKeyStats(sym)
-    stocksQuote = iex.stocksQuote(sym)
-    db[sym].insert({'stocksKeyStats': stocksKeyStats,
-        'stocksFinancials': stocksFinancials,
-        'stocksEarnings': stocksEarnings,
-        'stocksQuote': stocksQuote})
+def finviz(alist):
+    for sym in alist:
+        if not sym.startswith('A'):
+            continue
+        print(sym)
+        f = Finviz()
+        jobj = f.get_stat(sym)
+        id = DB[sym].find_one({}, {"_id": 1})
+        DB[sym].update_one({"_id": ObjectId(id['_id'])}, {"$set": {'finviz': jobj}})
+
+if __name__ == "__main__":
+    iex = IEX()
+    # iex_database(iex.symbols())
+    finviz(iex.symbols())
---
+++ stockprice/lib/finviz.py
@@ -0,0 +1,24 @@
+import requests
+
+import json
+
+class Finviz:
+    URL = "http://localhost:8080/finance/finviz/statistics"
+
+    def __init__(self):
+        pass
+
+    def get_stat(self, symbol):
+        url = "{}/{}".format(self.URL, symbol)
+        r = requests.get(url)
+        if r.status_code == 200:
+            astr = r.text
+            astr = astr.replace("Oper. Margin", "Oper Margin")
+            jobj = json.loads(astr)
+            return jobj
+        else:
+            return None
+
+if __name__ == "__main__":
+    f = Finviz()
+    print(f.get_stat('A'))
