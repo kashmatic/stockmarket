131543968b76b660238c5183481107e48580eceb
Kashi Revanna <kashi.revanna@verizonwireless.com>
2018-02-26 22:32:19 -0600

    (update) database update

--- website/app/routes.py
+++ website/app/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, make_response, request
 import mysql.connector as MS
 # DB = MS.connect(host="localhost", user="root", passwd="", database='stockmarket')
 DB = MS.connect(host="localhost", user="root", passwd="J3sus0MA!!", database='stockmarket')
+
 CURSOR = DB.cursor()
 
 @app.route('/')
