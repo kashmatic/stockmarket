cacfd661c7e2a50b99e0f8396dfce2a58992940d
Kashi Revanna <kashi.revanna@verizonwireless.com>
2018-03-01 10:57:34 -0600

    (update) database update

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "AlphaBeta"
+app.config['WTF_CSRF_SECRET_KEY'] = "SecretKey"
+
 from app import routes
--- website/app/routes.py
+++ website/app/routes.py
@@ -1,23 +1,49 @@
 from app import app
 from flask import render_template, make_response, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 import mysql.connector as MS
-# DB = MS.connect(host="localhost", user="root", passwd="", database='stockmarket')
-DB = MS.connect(host="localhost", user="root", passwd="J3sus0MA!!", database='stockmarket')
+DB = MS.connect(host="localhost", user="root", passwd="", database='stockmarket')
+# DB = MS.connect(host="localhost", user="root", passwd="J3sus0MA!!", database='stockmarket')
 
 CURSOR = DB.cursor()
 
+class FilterForm(FlaskForm):
+    marketCap = StringField('Market Cap:', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+def setfilter(obj):
+    alist = []
+    for key in obj:
+        if 'marketcap' in key:
+            alist.append("marketCap > {}".format(obj[key]))
+    return " AND ".join(alist)
+
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
     # return 'Hello, World!!'
-    print(request.cookies)
+    # print(request.cookies)
+    form = FilterForm(request.form)
     alist = []
+    if form.validate():
+        val = request.form.get('marketCap')
+        if val:
+            alist.append("marketCap > {}".format(val))
+        print(alist)
+
+
     try:
-        CURSOR.execute('SELECT * FROM stocks')
+        if alist:
+            sql = 'SELECT * FROM stocks WHERE {}'.format(" AND ".join(alist))
+        else:
+            sql = 'SELECT * FROM stocks'
+        print(sql)
+        CURSOR.execute(sql)
         alist = CURSOR.fetchall()
     except Exception as e:
         print(e.msg)
-    resp = make_response(render_template('hello.html', alist=alist))
-    resp.set_cookie('cookie_name',value='values')
-    return resp
+
+    return render_template('hello.html', alist=alist, form=form)
